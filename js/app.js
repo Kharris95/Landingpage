e5ea59e80c23ce9ddefa531df443e579b4b368c9
Keith Harris <keith_harris@live.com>
2024-11-02 21:42:16 -0400

    Update app.js
    Fleshing out navigation and active functionality

--- js/app.js
+++ js/app.js
@@ -17,40 +17,70 @@
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
 */
-
+const navigation = document.getElementById('navbar__list')
+const sections = document.querySelectorAll('section');
 /**
- * Define Global Variables
- * 
+* Define Global Variables
+/**
+
+* End Global Variables
+* Start Helper Functions
+* 
 */
 
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
 
 /**
- * End Helper Functions
- * Begin Main Functions
- * 
+* End Helper Functions
+* Begin Main Functions
+* 
 */
 
 // build the nav
+const navBuilder=() => {
+  let navUI=''
+  sections.forEach(section => {
+      const sectionID = section.id;
+      const sectionDataNav = section.dataset.nav;
+      navUI += `<li><a class="menuLink" href="#${sectionID}">${sectionDataNav}</a></li>`;
+  });
+  navigation.innerHTML=navUI;   
 
+};
+navBuilder();
 
 // Add class 'active' to section when near top of viewport
-
-
+// obtaining value closest to number
+const bounding = (section) => {
+  return Math.floor(section.getBoundingClientRect().top);
+};
+//removes active class
+const removeActiveClass = (section) =>{
+  section.classList.remove('your-active-class');
+};
+// adds active class
+const addActive = (cond,section) => {
+  if(cond){
+      section.classList.add('your-active-class');
+  };
+};
+// putting both together for the function
+const activateSection = () =>{
+  sections.forEach(section => { 
+      const boundingSection = bounding(section);
+      inviewport = () => boundingSection <120 && boundingSection >= -120;
+      removeActiveClass(section);
+      addActive(inviewport(),section);
+  });
+};
+window.addEventListener('scroll',activateSection);
 // Scroll to anchor ID using scrollTO event
 
 
 /**
- * End Main Functions
- * Begin Events
- * 
+* End Main Functions
+* Begin Events
+* 
 */
 
 // Build menu 
@@ -58,5 +88,4 @@
 // Scroll to section on link click
 
 // Set sections as active
-
 
